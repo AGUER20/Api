cbd5709da5d1980993fd883309aa46625099b8d0
AGUER20 <abraham.aguer@strathmore.edu>
2024-11-22 13:16:55 +0300

    Tested uing thunderclient

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(requeest):
+    return  HttpResponse("welcome")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
-]
+    path("", home_view)
+
+]   
